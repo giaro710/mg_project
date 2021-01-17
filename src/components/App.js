86f2137f14dd97fbf15af2ab5bd494d102fe1476
Gian Marco Romani <giaro710@gmail.com>
2021-01-17 17:41:44 +0100

    Added a state hook in the app to handle the token and make the right component to appear

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Login from './Login';
 import Landing from './Landing';
 
 const App = () => {
+  const [token, setToken] = useState(false);
+
+  if (!token) {
+    return <Login />;
+    // N.B. Posso passare la funzione hook 'setToken' come props a login
+    // return <Login setToken={setToken}/>;
+    // In questo modo posso prendere il token da redux e poi settarlo direttamente nel component del login.
+  }
+
   return (
     <div>
       <BrowserRouter>
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -5,6 +5,14 @@ const Landing = () => {
   return (
     <div>
       <h1>Landing Page</h1>
+      <p>
+        This is the landing page. I write down this thing just to make the space
+        not so empty
+      </p>
+      <br />
+      <br />
+      <br />
+      <br />
       <Link to="/login">Vai al Login</Link>
     </div>
   );
